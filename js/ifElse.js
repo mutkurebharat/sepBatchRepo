c4bf727372e5ac5c4e74453a03084fcbfb39336d
BharatM <mutkurebharat@gmail.com>
2020-12-09 17:38:12 +0530

    Condiotanal Statement

--- js/ifElse.js
+++ js/ifElse.js
@@ -1,25 +1,18 @@
 var input = Number(prompt("Please Enter Some Number between 1 to 5"));
 
-if(input != 1 && input != 2 && input != 3 && input != 4 && input != 5) {
+if(input === 1) {
+    alert("You have entered 1");
+} else if(input === 2) {
+    alert("You have entered 2");
+} else if(input === 3) {
+    alert("You have entered 3");
+} else if(input === 4) {
+    alert("You have entered 4");
+} else if(input === 5) {
+    alert("You have entered 5");
+} else if(input != 1 && input != 2 && input != 3 && input != 4 && input != 5) {
     alert("Not a valid entry, Please enter a number between 1 to 5")
 }
 
-if(input === 1) {
-    alert("You have entered 1");
-}
 
-if(input === 2) {
-    alert("You have entered 2");
-}
 
-if(input === 3) {
-    alert("You have entered 3");
-}
-
-if(input === 4) {
-    alert("You have entered 4");
-}
-
-if(input === 5) {
-    alert("You have entered 5");
-}
---
+++ js/switchCase.js
@@ -0,0 +1,22 @@
+var input = Number(prompt("Please Enter Some Number between 1 to 5"));
+
+switch(input) {
+    case 1: 
+        alert("Your Number is 1");
+        break;
+    case 2:
+        alert("Your Number is 2");
+        break;
+    case 3:
+        alert("Your Number is 3");
+        break;
+    case 4:
+        alert("Your Number is 4");
+        break;
+    case 5:
+        alert("Your Number is 5");
+        break;
+    default: 
+    alert("Not a valid entry, Please enter a number between 1 to 5");
+    break;
+}
